3a1bd07421928f02cf928a5436b3feee82dcd938
Archie Baer <archie@abaer.dev>
2020-01-15 21:05:48 +0000

    Keyboard shortcut + comments

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ const electron = require("electron");
 
 var inputWindow;
 const createInputWindow = () => {
+
+    //Prevent Duplicate Input Windows
+    if (inputWindow) return;
+
+    //Create Window
     inputWindow = new electron.BrowserWindow({
         width: 800,
         height: 75,
@@ -13,9 +18,20 @@ const createInputWindow = () => {
     });
     inputWindow.loadFile("input/index.html");
 
+    //Close on blur
     inputWindow.on("blur", () => {
         //win.close();
     });
+
+    inputWindow.on("close", () => {
+        inputWindow = null;
+    });
 }
 
-electron.app.on("ready", createInputWindow);
+electron.app.on("ready", () => {
+    //Keyboard Shortcut
+    electron.globalShortcut.register("CommandOrControl+Shift+A", createInputWindow);
+});
+
+//Prevent stopping app when windows close
+electron.app.on("window-all-closed", e => e.preventDefault());
