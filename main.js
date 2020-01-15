c6e75a93cb3ba2c7669dbfd10498f39e14a473dc
Archie Baer <archie@abaer.dev>
2020-01-15 20:32:28 +0000

    Node integration and close on blur

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 const electron = require("electron");
 
-function createInputWindow() {
+const createInputWindow = () => {
     const win = new electron.BrowserWindow({
         width: 800,
         height: 75,
-        frame: false
+        frame: false,
+        webPreferences: {
+            nodeIntegration: true
+        }
     });
     win.loadFile("input/index.html");
+    win.on("blur", () => {
+        //win.close();
+    });
 }
 
 electron.app.on("ready", createInputWindow);
